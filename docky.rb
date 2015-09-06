f0ecc0c42f1196626a9be534a0c0707e8cd946eb
Michael Limiero <mike5713@gmail.com>
2015-09-06 18:29:44 -0400

    Start, stop, restart, launch, relaunch

--- docky.rb
+++ docky.rb
@@ -8,6 +8,11 @@ Docky 0.1
 
 Usage:
   docky [options] check [NAME ...]
+  docky [options] start NAME ...
+  docky [options] stop NAME ...
+  docky [options] restart NAME ...
+  docky [options] launch NAME ...
+  docky [options] relaunch NAME ...
 
 Options:
   -h, --help                   Show this screen.
@@ -49,76 +54,164 @@ def prepare
   end
 end
 
-def check
-  $opts['NAME'].each do |name|
-    obj = $conf[name]
-    if obj.nil?
-      puts "#{name} not found in config file"
-      next
-    end
+def check(name)
+  obj = $conf[name]
+  if obj.nil?
+    puts "#{name} not found in config file"
+    return
+  end
 
-    if obj["enabled"] == false
-      puts "#{name} is running, should be stopped" if $info[name]["State"]["Running"]
+  if obj["enabled"] == false
+    puts "#{name} is running, should be stopped" if $info[name]["State"]["Running"]
+  else
+    puts "#{name} is stopped, should be running" unless $info[name]["State"]["Running"]
+  end
+
+  if obj["image"]
+    if $images[obj["image"]].nil?
+      puts "#{name}: image #{obj["image"]} does not exist"
+    elsif $images[obj["image"]]["Id"] != $info[name]["Image"]
+      puts "#{name} is not running image #{obj["image"]}"
+    end
+  end
+
+  if obj["data"]
+    if $info[obj["data"]].nil?
+      puts "#{name} data container #{obj["data"]} does not exist"
     else
-      puts "#{name} is stopped, should be running" unless $info[name]["State"]["Running"]
+      puts "#{name} does not have volumes from #{obj["data"]}" unless $info[name]["HostConfig"]["VolumesFrom"].include? obj["data"]
     end
+  end
 
-    if obj["image"]
-      if $images[obj["image"]].nil?
-        puts "#{name}: image #{obj["image"]} does not exist"
-      elsif $images[obj["image"]]["Id"] != $info[name]["Image"]
-        puts "#{name} is not running image #{obj["image"]}"
-      end
-    end
-
-    if obj["data"]
-      if $info[obj["data"]].nil?
-        puts "#{name} data container #{obj["data"]} does not exist"
+  unless obj["ports"].nil? or !$info[name]["State"]["Running"]
+    obj["ports"].each do |port|
+      a = port.split(":")
+      if a.length == 1
+        p = {hostIp: "0.0.0.0", hostPort: "", containerPort: a[0]}
+      elsif a.length == 2
+        p = {hostIp: "0.0.0.0", hostPort: a[0], containerPort: a[1]}
+      elsif a.length == 3
+        p = {hostIp: a[0], hostPort: a[1], containerPort: a[2]}
       else
-        puts "#{name} does not have volumes from #{obj["data"]}" unless $info[name]["HostConfig"]["VolumesFrom"].include? obj["data"]
+        puts "invalid portspec #{port}"
+        return
       end
-    end
-
-    unless obj["ports"].nil? or !$info[name]["State"]["Running"]
-      obj["ports"].each do |port|
-        a = port.split(":")
-        if a.length == 1
-          p = {hostIp: "0.0.0.0", hostPort: "", containerPort: a[0]}
-        elsif a.length == 2
-          p = {hostIp: "0.0.0.0", hostPort: a[0], containerPort: a[1]}
-        elsif a.length == 3
-          p = {hostIp: a[0], hostPort: a[1], containerPort: a[2]}
-        else
-          puts "invalid portspec #{port}"
-          next
-        end
-        p[:containerPort] = p[:containerPort]+"/tcp" unless p[:containerPort].include? "/"
-        p2 = $info[name]["NetworkSettings"]["Ports"][p[:containerPort]]
-        if p2.nil? or p2[0]["HostIp"] != p[:hostIp] or p[:hostPort] != "" && p2[0]["HostPort"] != p[:hostPort]
-          puts "#{name} does not have port #{port}"
-        end
-      end
-    end
-
-    unless obj["env"].nil?
-      obj["env"].each do |k,v|
-        unless $info[name]["Config"]["Env"].include? "#{k}=#{v}"
-          puts "#{name} does not have env #{k}=#{v}"
-        end
-      end
-    end
-
-    unless obj["links"].nil?
-      obj["links"].each do |link|
-        exname, _, inname = link.partition(":")
-        exname = "/#{exname}" unless exname.start_with? "/"
-        inname = "/#{name}/#{inname}" unless inname.start_with? "/"
-        unless $info[name]["HostConfig"]["Links"].include? "#{exname}:#{inname}"
-          puts "#{name} does not have link #{link}"
-        end
+      p[:containerPort] = p[:containerPort]+"/tcp" unless p[:containerPort].include? "/"
+      p2 = $info[name]["NetworkSettings"]["Ports"][p[:containerPort]]
+      if p2.nil? or p2[0]["HostIp"] != p[:hostIp] or p[:hostPort] != "" && p2[0]["HostPort"] != p[:hostPort]
+        puts "#{name} does not have port #{port}"
       end
     end
   end
+
+  unless obj["env"].nil?
+    obj["env"].each do |k,v|
+      unless $info[name]["Config"]["Env"].include? "#{k}=#{v}"
+        puts "#{name} does not have env #{k}=#{v}"
+      end
+    end
+  end
+
+  unless obj["links"].nil?
+    obj["links"].each do |link|
+      exname, _, inname = link.partition(":")
+      exname = "/#{exname}" unless exname.start_with? "/"
+      inname = "/#{name}/#{inname}" unless inname.start_with? "/"
+      unless $info[name]["HostConfig"]["Links"].include? "#{exname}:#{inname}"
+        puts "#{name} does not have link #{link}"
+      end
+    end
+  end
+end
+
+def start(name)
+  puts `#{$opts['--docker']} start #{name}`
+end
+
+def stop(name)
+  puts `#{$opts['--docker']} stop #{name}`
+end
+
+def restart(name)
+  stop(name)
+  start(name)
+end
+
+def launch(name)
+  obj = $conf[name]
+  if $info[name]
+    puts "#{name} already exists! Did you mean relaunch?"
+    return
+  end
+  str = "#{$opts['--docker']} run --name #{name} -t -i -d"
+  if obj["data"]
+    str += " --volumes-from #{obj["data"]}"
+  end
+  if obj["ports"]
+    obj["ports"].each { |port| str += " -p #{port}" }
+  end
+  if obj["env"]
+    obj["env"].each { |k,v| str += " -e #{k}=#{v}" }
+  end
+  if obj["links"]
+    obj["links"].each { |link| str += " --link #{link}" }
+  end
+  str += " #{obj["image"]}"
+  puts str
+  puts `#{str}`
+end
+
+def destroy(name)
+  obj = $conf[name]
+  if $info[name].nil?
+    puts "#{name} does not seem to exist"
+    return
+  end
+  if obj["image"].nil?
+    puts "#{name} has no image"
+    puts "cowardly refusing to continue"
+    return
+  end
+  if obj["data"] and $info[obj["data"]].nil?
+    puts "data container #{obj["data"]} does not seem to exist"
+    puts "cowardly refusing to continue"
+    return
+  end
+  stop(name) if $info[name]["State"]["Running"]
+  puts `#{$opts['--docker']} rm #{name}`
+  $info.delete(name)
+end
+
+def relaunch(name)
+  if $info[name].nil?
+    puts "#{name} does not seem to exist"
+    return
+  end
+
+  # get all the current ports
+  if $conf[name]["ports"]
+    $conf[name]["ports"].map! do |port|
+      a = port.split(":")
+      if a.length == 1
+        p = {hostIp: "0.0.0.0", hostPort: "", containerPort: a[0]}
+      elsif a.length == 2
+        p = {hostIp: "0.0.0.0", hostPort: a[0], containerPort: a[1]}
+      elsif a.length == 3
+        p = {hostIp: a[0], hostPort: a[1], containerPort: a[2]}
+      else
+        puts "invalid portspec #{port}"
+        return
+      end
+      p[:containerPort] = p[:containerPort]+"/tcp" unless p[:containerPort].include? "/"
+      p2 = $info[name]["NetworkSettings"]["Ports"][p[:containerPort]][0]
+      if p[:hostPort].empty? and !p2.nil?
+        p[:hostPort] = p2["HostPort"]
+      end
+      "#{p[:hostIp]}:#{p[:hostPort]}:#{p[:containerPort]}"
+    end
+  end
+  destroy(name)
+  launch(name)
 end
 
 begin
@@ -133,7 +226,19 @@ begin
 
   if $opts['check']
     prepare
-    check
+    $opts['NAME'].each {|name| check(name)}
+  elsif $opts['start']
+    $opts['NAME'].each {|name| start(name)}
+  elsif $opts['stop']
+    $opts['NAME'].each {|name| stop(name)}
+  elsif $opts['restart']
+    $opts['NAME'].each {|name| restart(name)}
+  elsif $opts['launch']
+    prepare
+    $opts['NAME'].each {|name| launch(name)}
+  elsif $opts['relaunch']
+    prepare
+    $opts['NAME'].each {|name| relaunch(name)}
   end
 rescue Docopt::Exit => e
   puts e.message
